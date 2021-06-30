6dd2b3e1375ef0c6d75ad21a162511cbec2b8c1e
angel.gimenez <angel.gimenez@bc3research.org>
2021-06-30 11:07:58 +0200

    Earthstat: sum harvested area all crops

---
+++ Geoprocessing/earthstat_data.py
@@ -0,0 +1,20 @@
+import rasterio
+import glob
+from dask_rasterio import read_raster, write_raster
+import dask.array as da
+
+earthstat_dir  = "C:/Users/angel/DATA/Earthstat/HarvestedAreaYield175Crops_Geotiff/HarvestedAreaYield175Crops_Geotiff/"
+layer = "HarvestedAreaHectares"
+ext = ".tif"
+selected_files = [file for file in glob.iglob(earthstat_dir + '**/*' + layer + ext, recursive=True)]
+map2array=[]
+for raster in selected_files:
+    map2array.append(read_raster(raster))
+
+ds_stack = da.stack(map2array)
+with rasterio.open(selected_files[0]) as src:
+    profile = src.profile
+    profile.update(compress='lzw')
+
+write_raster(earthstat_dir + "SumHarvestedAreaHectares.tif", da.nansum(ds_stack,0), **profile)
+
